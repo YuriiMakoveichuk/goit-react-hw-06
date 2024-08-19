6ad7bedeb19dfdd68d94fad23146a79bd0ecf7df
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-08-19 18:41:00 +0300

    added components and contacts.json

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,7 @@
+import css from "./Contact.module.css";
+
+const Contact = () => {
+  return <div>Contact</div>;
+};
+
+export default Contact;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,12 @@
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+
+const ContactList = () => {
+  return (
+    <>
+      <Contact />
+    </>
+  );
+};
+
+export default ContactList;
